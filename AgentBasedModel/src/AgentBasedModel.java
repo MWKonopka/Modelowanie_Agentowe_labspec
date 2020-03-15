013744c6643381ef7ca5f13dd32d36e065344fb1
MWKonopka <makonwi@gmail.com>
2020-03-15 21:47:03 +0100

    Commit1

---
+++ AgentBasedModel/src/AgentBasedModel.java
@@ -0,0 +1,14 @@
+import graph.Graph;
+
+public class AgentBasedModel {
+	
+	public static void main(String [] args) {
+	 System.out.println("ER graph :");
+	 
+	 Graph graph = new Graph(10,0.2);
+	 graph.printParameters();
+	 graph.printAdjcencencyList();
+	 graph.printComponents();
+	}
+	
+}
---
+++ AgentBasedModel/src/graph/Graph.java
@@ -0,0 +1,119 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+
+
+public class Graph {
+
+	private int numOfVertices;
+	private double probOfConnection;
+	private List<ArrayList<Integer>> adjencencyList;
+	private List<ArrayList<Integer>> components;
+	private boolean visited[];
+	private int numOfComponents;
+	
+	public Graph(int nVertices, double probOfConnect)			//Tworzenie listy sąsiedztwa
+	{
+		this.numOfVertices = nVertices;
+		this.probOfConnection = probOfConnect;
+		adjencencyList = new ArrayList<ArrayList<Integer>>();
+		components = new ArrayList<ArrayList<Integer>>();
+		numOfComponents = 0;
+		
+			visited = new boolean[numOfVertices];
+			for(int i = 0; i < numOfVertices; i++)
+				visited[i] = false;
+																
+		for(int i = 0; i< this.numOfVertices; i++)
+		{
+		 ArrayList<Integer> aux = new ArrayList<Integer>();
+		 adjencencyList.add(aux);
+		}
+																		
+		for(int i = 0; i< numOfVertices; i++)
+			for(int j = i+1; j<numOfVertices; j++)
+			{
+		    Random p = new Random();
+				if(p.nextDouble() < probOfConnection)
+				{
+					adjencencyList.get(i).add(j);
+					adjencencyList.get(j).add(i);
+				}
+			}
+		this.getComponents();
+	}
+	
+	
+	public void printParameters()
+	{
+		System.out.println("V: " + numOfVertices    + "\n" + 
+						   "p: " + probOfConnection + "\n" );
+	}
+	
+	
+	public void printAdjcencencyList()
+	{
+	System.out.println("Adjencency list:");
+		for(int i = 0; i<numOfVertices; i++)
+		{
+			System.out.print(i + ":[ ");
+			for(int j = 0 ;j < adjencencyList.get(i).size(); j++)
+				System.out.print(adjencencyList.get(i).get(j) + " ");
+			System.out.println("]");
+		}	
+	}
+	
+	
+	private ArrayList<Integer> dfs(int node) 					//implementacja algorytmu depth-first-search
+	{
+		
+		ArrayList<Integer> component = new ArrayList<Integer>();
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.add(node);
+			while(!stack.isEmpty())
+			{
+			 Integer first_out = stack.pop();
+			 if(!visited[first_out])
+			 {
+				 visited[first_out] = true;
+			 	 component.add(first_out);
+			 }
+			 	for(int i=0; i<adjencencyList.get(first_out).size();i++)
+			 	{
+				 int neighbor = adjencencyList.get(first_out).get(i);
+				 if(!visited[neighbor])
+					 stack.add(neighbor);
+			 	}
+			}	
+		return component;
+	}
+	
+	
+	public void getComponents()						//Zwraca liste componentow
+	{
+	
+	 for(int i=0; i<numOfVertices; i++)
+		 if(!visited[i])
+		 {
+			 components.add(dfs(i));
+			 numOfComponents++;
+		 }
+	}
+	
+	public void printComponents()
+	{
+	 System.out.println(numOfComponents + " components:");
+		for(int i=0; i<components.size(); i++)
+		{
+			System.out.print(i + " {");
+			for(int j=0; j<components.get(i).size(); j++)
+				System.out.print(components.get(i).get(j)+ " ");
+			System.out.println("}");
+		}
+				
+	}
+}
